af815778ddec508e0978a4cd9bf49a836c3cb3d1
ukey77 <yujin25683@gmail.com>
2025-08-04 15:34:17 +0900

    first commit

---
+++ SpringStart/src/main/java/kr/bit/controller/HomeController.java
@@ -0,0 +1,31 @@
+package kr.bit.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping("/")
+	public String home(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home"; //home.jsp
+	}
+	
+}
